lustre/lustre-release

commit 0006eb36440dcb4dc06aa61c35db40bf7dec0ddc

LU-16328 llite: migrate_folio, vfs_setxattr
Linux commit v5.19-rc3-392-g5490da4f06d1
 fs: Add aops->migrate_folio

Linux commit v5.19-rc4-52-ge33c267ab70d
  mm: shrinkers: provide shrinkers with names

From Linux commit v5.19-rc5-17-g0c5fd887d2bb
  acl: move idmapped mount fixup into vfs_{g,s}etxattr()
Until Linux commit v6.0-rc3-6-g6344e66970c6
  xattr: constify value argument in vfs_setxattr()
Cast away const when required.

Linux commit v5.19-10313-geba2d3d79829
  get rid of non-advancing variants
iov_iter_get_pages_alloc2() replaces iov_iter_get_pages_alloc()

Test-Parameters: trivial
HPE-bug-id: LUS-11358
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Id1fa6db94172c0a61008ba4b066907950bdd6473
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49265
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: xinliang <xinliang.liu@linaro.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_compat.h
@@ -510,7 +510,8 @@ static inline int ll_vfs_setxattr(struct dentry *dentry, struct inode *inode,
 				  const void *value, size_t size, int flags)
 {
 #ifdef HAVE_USER_NAMESPACE_ARG
-	return vfs_setxattr(&init_user_ns, dentry, name, value, size, flags);
+	return vfs_setxattr(&init_user_ns, dentry, name,
+			    VFS_SETXATTR_VALUE(value), size, flags);
 #elif defined(HAVE_VFS_SETXATTR)
 	return __vfs_setxattr(dentry, inode, name, value, size, flags);
 #else
@@ -559,7 +560,21 @@ static inline bool is_root_inode(struct inode *inode)
 }
 #endif
 
-#ifndef HAVE_REGISTER_SHRINKER_RET
+#ifndef HAVE_IOV_ITER_GET_PAGES_ALLOC2
+#define iov_iter_get_pages_alloc2(i, p, m, s) \
+	iov_iter_get_pages_alloc((i), (p), (m), (s))
+#endif
+
+#ifdef HAVE_AOPS_MIGRATE_FOLIO
+#define folio_migr	folio
+#else
+#define folio_migr	page
+#define migrate_folio	migratepage
+#endif
+
+#ifdef HAVE_REGISTER_SHRINKER_FORMAT_NAMED
+#define register_shrinker(_s) register_shrinker((_s), "%ps", (_s))
+#elif !defined(HAVE_REGISTER_SHRINKER_RET)
 #define register_shrinker(_s) (register_shrinker(_s), 0)
 #endif
 

lustre/llite/rw26.c
@@ -255,11 +255,7 @@ static ssize_t ll_get_user_pages(int rw, struct iov_iter *iter,
 	size_t start;
 	size_t result;
 
-	/*
-	 * iov_iter_get_pages_alloc() is introduced in 3.16 similar
-	 * to HAVE_DIO_ITER.
-	 */
-	result = iov_iter_get_pages_alloc(iter, pages, maxsize, &start);
+	result = iov_iter_get_pages_alloc2(iter, pages, maxsize, &start);
 	if (result > 0)
 		*npages = DIV_ROUND_UP(result + start, PAGE_SIZE);
 
@@ -989,12 +985,12 @@ out:
 }
 
 #ifdef CONFIG_MIGRATION
-static int ll_migratepage(struct address_space *mapping,
-			  struct page *newpage, struct page *page,
-			  enum migrate_mode mode)
+static int ll_migrate_folio(struct address_space *mapping,
+			    struct folio_migr *newpage, struct folio_migr *page,
+			    enum migrate_mode mode)
 {
-        /* Always fail page migration until we have a proper implementation */
-        return -EIO;
+	/* Always fail page migration until we have a proper implementation */
+	return -EIO;
 }
 #endif
 
@@ -1025,6 +1021,6 @@ const struct address_space_operations ll_aops = {
 	.write_begin		= ll_write_begin,
 	.write_end		= ll_write_end,
 #ifdef CONFIG_MIGRATION
-	.migratepage		= ll_migratepage,
+	.migrate_folio		= ll_migrate_folio,
 #endif
 };

lustre/autoconf/lustre-core.m4
@@ -3391,6 +3391,112 @@ AC_DEFUN([LC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO], [
 ]) # LC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
 
 #
+# LC_HAVE_ADDRESS_SPACE_OPERATIONS_MIGRATE_FOLIO
+#
+# Linux commit v5.19-rc3-392-g5490da4f06d1
+#  fs: Add aops->migrate_folio
+#
+AC_DEFUN([LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_MIGRATE_FOLIO], [
+	LB2_LINUX_TEST_SRC([address_space_operations_migrate_folio], [
+		#include <linux/fs.h>
+	],[
+		struct address_space_operations *aops = NULL;
+		struct address_space *m = NULL;
+		struct folio *src = NULL;
+		struct folio *dst = NULL;
+		int err = aops->migrate_folio(m, dst, src, MIGRATE_ASYNC);
+		(void)err;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_ADDRESS_SPACE_OPERATIONS_MIGRATE_FOLIO], [
+	AC_MSG_CHECKING([if struct address_space_operations() has migrate_folio()])
+	LB2_LINUX_TEST_RESULT([address_space_operations_migrate_folio], [
+		AC_DEFINE(HAVE_AOPS_MIGRATE_FOLIO, 1,
+			[struct address_space_operations() has migrate_folio()])
+	])
+]) # LC_HAVE_ADDRESS_SPACE_OPERATIONS_MIGRATE_FOLIO
+
+#
+# LC_REGISTER_SHRINKER_FORMAT_NAMED
+#
+# Linux commit v5.19-rc4-52-ge33c267ab70d
+#   mm: shrinkers: provide shrinkers with names
+#
+AC_DEFUN([LC_SRC_REGISTER_SHRINKER_FORMAT_NAMED], [
+	LB2_LINUX_TEST_SRC([register_shrinker_format], [
+		#include <linux/mm.h>
+	],[
+		if (register_shrinker(NULL, "lustre-%ps", __func__))
+			unregister_shrinker(NULL);
+	],[-Werror])
+])
+AC_DEFUN([LC_REGISTER_SHRINKER_FORMAT_NAMED], [
+	AC_MSG_CHECKING([if register_shrinker() returns status])
+	LB2_LINUX_TEST_RESULT([address_space_operations_migrate_folio], [
+		AC_DEFINE(HAVE_REGISTER_SHRINKER_FORMAT_NAMED, 1,
+			[register_shrinker() returns status])
+	])
+]) # LC_REGISTER_SHRINKER_FORMAT_NAMED
+
+#
+# LC_HAVE_VFS_SETXATTR_NON_CONST_VALUE
+#
+# From Linux commit v5.19-rc5-17-g0c5fd887d2bb
+#   acl: move idmapped mount fixup into vfs_{g,s}etxattr()
+# Until Linux commit v6.0-rc3-6-g6344e66970c6
+#   xattr: constify value argument in vfs_setxattr()
+#
+AC_DEFUN([LC_SRC_HAVE_VFS_SETXATTR_NON_CONST_VALUE], [
+	LB2_LINUX_TEST_SRC([vfs_setxattr_non_const_value_arg], [
+		#include <linux/xattr.h>
+	],[
+		struct dentry *de = NULL;
+		const char *name = "an.xattr";
+		const void *value = NULL;
+		int err = vfs_setxattr(&init_user_ns, de, name, value, 0, 0);
+		(void)err;
+	],[-Werror])
+]) # LC_HAVE_VFS_SETXATTR_NON_CONST_VALUE
+AC_DEFUN([LC_HAVE_VFS_SETXATTR_NON_CONST_VALUE], [
+	AC_MSG_CHECKING([if vfs_setxattr() value argument is non-const])
+	LB2_LINUX_TEST_RESULT([vfs_setxattr_non_const_value_arg], [
+		AC_DEFINE([VFS_SETXATTR_VALUE(value)],
+			  [(value)],
+			  [vfs_setxattr() value argument is const void *])
+	],[
+		AC_DEFINE([VFS_SETXATTR_VALUE(value)],
+			  [((void *)(value))],
+			  [vfs_setxattr() value argument is non-const])
+	])
+]) # LC_HAVE_VFS_SETXATTR_NON_CONST_VALUE
+
+#
+# LC_HAVE_IOV_ITER_GET_PAGES_ALLOC2
+#
+# Linux commit v5.19-10313-geba2d3d79829
+#   get rid of non-advancing variants
+#
+AC_DEFUN([LC_SRC_HAVE_IOV_ITER_GET_PAGES_ALLOC2], [
+	LB2_LINUX_TEST_SRC([iov_iter_get_pages_alloc2], [
+		#include <linux/uio.h>
+	],[
+		struct iov_iter *iter = NULL;
+		struct page ***pages = NULL;
+		size_t maxsize = 1;
+		size_t start;
+		size_t result __attribute__ ((unused));
+		result = iov_iter_get_pages_alloc2(iter, pages, maxsize, &start);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_IOV_ITER_GET_PAGES_ALLOC2], [
+	AC_MSG_CHECKING([if iov_iter_get_pages_alloc2() is available])
+	LB2_LINUX_TEST_RESULT([iov_iter_get_pages_alloc2], [
+		AC_DEFINE(HAVE_IOV_ITER_GET_PAGES_ALLOC2, 1,
+			[iov_iter_get_pages_alloc2() is available])
+	])
+]) # LC_HAVE_IOV_ITER_GET_PAGES_ALLOC2
+
+#
 # LC_PROG_LINUX
 #
 # Lustre linux kernel checks
@@ -3614,6 +3720,12 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_READ_FOLIO
 	LC_SRC_HAVE_READ_CACHE_PAGE_FILLER_WITH_FILE
 	LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
+
+	# 6.0
+	LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_MIGRATE_FOLIO
+	LC_SRC_REGISTER_SHRINKER_FORMAT_NAMED
+	LC_SRC_HAVE_VFS_SETXATTR_NON_CONST_VALUE
+	LC_SRC_HAVE_IOV_ITER_GET_PAGES_ALLOC2
 
 	# kernel patch to extend integrity interface
 	LC_SRC_BIO_INTEGRITY_PREP_FN
@@ -3853,6 +3965,12 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_ADDRESS_SPACE_OPERATIONS_READ_FOLIO
 	LC_HAVE_READ_CACHE_PAGE_FILLER_WITH_FILE
 	LC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
+
+	# 6.0
+	LC_HAVE_ADDRESS_SPACE_OPERATIONS_MIGRATE_FOLIO
+	LC_REGISTER_SHRINKER_FORMAT_NAMED
+	LC_HAVE_VFS_SETXATTR_NON_CONST_VALUE
+	LC_HAVE_IOV_ITER_GET_PAGES_ALLOC2
 
 	# kernel patch to extend integrity interface
 	LC_BIO_INTEGRITY_PREP_FN

stancheff/lustre

commit 538f673c201782c1b1a6e5f6b4e836acd832789d

LU-17085 llite: safely duplicate iov_iter
When copying an iov_iter the underlying iovec/bvec also
needs to be sized duplicated.
Note that discard and xarray iters can be skipped here,
however the duplicated vector needs to be tracked and freed.

This borrows heavily from dup_iter()

the iter argument to ll_dio_user_copy() to no longer used
and can be removed.

Also addeded tests for aligned and unaligned readv/writev
and fixes the use of iov_iter_get_pages_alloc2() which
advances the iter when iov_iter_get_pages_alloc() did not.

Updated ll_get_user_pages to revert the advanced iter

Fixes: 0006eb3644 ("LU-16328 llite: migrate_folio, vfs_setxattr")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I9f68eaa14abc8915d543dba91eea598edbd9872d

lustre/include/cl_object.h
@@ -2584,6 +2584,11 @@ struct cl_dio_aio {
 				cda_creator_free:1;
 };
 
+struct cl_iter_dup {
+	void		*id_vec;             /* dup'd vec (iov/bvec/kvec) */
+	size_t		id_vec_size;         /* bytes allocated for id_vec */
+};
+
 /* Sub-dio used for splitting DIO (and AIO, because AIO is DIO) according to
  * the layout/striping, so we can do parallel submit of DIO RPCs
  */
@@ -2594,6 +2599,7 @@ struct cl_sub_dio {
 	struct cl_dio_aio	*csd_ll_aio;
 	struct ll_dio_pages	csd_dio_pages;
 	struct iov_iter		csd_iter;
+	struct cl_iter_dup	csd_dup;
 	unsigned		csd_creator_free:1,
 				csd_write:1,
 				csd_unaligned:1;
@@ -2606,7 +2612,7 @@ struct cl_sub_dio {
 void ll_release_user_pages(struct page **pages, int npages);
 int ll_allocate_dio_buffer(struct ll_dio_pages *pvec, size_t io_size);
 void ll_free_dio_buffer(struct ll_dio_pages *pvec);
-ssize_t ll_dio_user_copy(struct cl_sub_dio *sdio, struct iov_iter *write_iov);
+ssize_t ll_dio_user_copy(struct cl_sub_dio *sdio);
 
 #ifndef HAVE_KTHREAD_USE_MM
 #define kthread_use_mm(mm) use_mm(mm)

lustre/include/lustre_compat.h
@@ -634,8 +634,19 @@ static inline bool is_root_inode(struct inode *inode)
 #endif
 
 #ifndef HAVE_IOV_ITER_GET_PAGES_ALLOC2
-#define iov_iter_get_pages_alloc2(i, p, m, s) \
-	iov_iter_get_pages_alloc((i), (p), (m), (s))
+static inline
+ssize_t iov_iter_get_pages_alloc2(struct iov_iter *i, struct page ***pages,
+				  size_t maxsize, size_t *start)
+{
+	ssize_t result;
+
+	/* iov_iter_get_pages_alloc is non advancing version of alloc2 */
+	result = iov_iter_get_pages_alloc(i, pages, maxsize, start);
+	if (result > 0)
+		iov_iter_advance(i, result);
+
+	return result;
+}
 #endif
 
 #ifdef HAVE_AOPS_MIGRATE_FOLIO

lustre/llite/rw26.c
@@ -252,9 +252,10 @@ static ssize_t ll_get_user_pages(int rw, struct iov_iter *iter,
 
 	result = iov_iter_get_pages_alloc2(iter, &pvec->ldp_pages, maxsize,
 					  &start);
-	if (result > 0)
+	if (result > 0) {
 		pvec->ldp_count = DIV_ROUND_UP(result + start, PAGE_SIZE);
-
+		iov_iter_revert(iter, result);
+	}
 	return result;
 #else
 	unsigned long addr;
@@ -622,7 +623,7 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 		}
 
 		if (unaligned && rw == WRITE) {
-			result = ll_dio_user_copy(sdio, iter);
+			result = ll_dio_user_copy(sdio);
 			if (unlikely(result <= 0)) {
 				cl_sync_io_note(env, &sdio->csd_sync, result);
 				if (sync_submit) {

lustre/obdclass/cl_io.c
@@ -1192,6 +1192,14 @@ static void cl_dio_aio_end(const struct lu_env *env, struct cl_sync_io *anchor)
 	EXIT;
 }
 
+static inline void csd_dup_free(struct cl_iter_dup *dup)
+{
+	void *tmp = dup->id_vec;
+
+	dup->id_vec = NULL;
+	OBD_FREE(tmp, dup->id_vec_size);
+}
+
 static void cl_sub_dio_end(const struct lu_env *env, struct cl_sync_io *anchor)
 {
 	struct cl_sub_dio *sdio = container_of(anchor, typeof(*sdio), csd_sync);
@@ -1208,11 +1216,6 @@ static void cl_sub_dio_end(const struct lu_env *env, struct cl_sync_io *anchor)
 	}
 
 	if (sdio->csd_unaligned) {
-		/* save the iovec pointer before it's modified by
-		 * ll_dio_user_copy
-		 */
-		struct iovec *tmp = (struct iovec *) sdio->csd_iter.__iov;
-
 		CDEBUG(D_VFSTRACE,
 		       "finishing unaligned dio %s aio->cda_bytes %ld\n",
 		       sdio->csd_write ? "write" : "read", sdio->csd_bytes);
@@ -1221,13 +1224,12 @@ static void cl_sub_dio_end(const struct lu_env *env, struct cl_sync_io *anchor)
 		 * buffer from userspace at the start
 		 */
 		if (!sdio->csd_write && sdio->csd_bytes > 0)
-			ret = ll_dio_user_copy(sdio, NULL);
+			ret = ll_dio_user_copy(sdio);
 		ll_free_dio_buffer(&sdio->csd_dio_pages);
 		/* handle the freeing here rather than in cl_sub_dio_free
 		 * because we have the unmodified iovec pointer
 		 */
-		OBD_FREE_PTR(tmp);
-		sdio->csd_iter.__iov = NULL;
+		csd_dup_free(&sdio->csd_dup);
 	} else {
 		/* unaligned DIO does not get user pages, so it doesn't have to
 		 * release them, but aligned I/O must
@@ -1294,7 +1296,9 @@ struct cl_sub_dio *cl_sub_dio_alloc(struct cl_dio_aio *ll_aio,
 
 		atomic_add(1,  &ll_aio->cda_sync.csi_sync_nr);
 
-		if (unaligned) {
+		if (sdio->csd_unaligned) {
+			size_t vec_size = 0;
+
 			/* we need to make a copy of the user iovec at this
 			 * point in time, in order to:
 			 *
@@ -1305,15 +1309,25 @@ struct cl_sub_dio *cl_sub_dio_alloc(struct cl_dio_aio *ll_aio,
 			 * modifies the iovec, so to process each chunk from a
 			 * separate thread requires a local copy of the iovec
 			 */
-			memcpy(&sdio->csd_iter, iter, sizeof(struct iov_iter));
-			OBD_ALLOC_PTR(sdio->csd_iter.__iov);
-			if (sdio->csd_iter.__iov == NULL) {
+			sdio->csd_iter = *iter;
+			if (iov_iter_is_bvec(iter))
+				vec_size = iter->nr_segs * sizeof(struct bio_vec);
+			else if (iov_iter_is_kvec(iter) || iter_is_iovec(iter))
+				vec_size = iter->nr_segs * sizeof(struct iovec);
+
+			/* xarray and discard do not need vec to be dup'd */
+			if (!vec_size)
+				goto out;
+
+			OBD_ALLOC(sdio->csd_dup.id_vec, vec_size);
+			if (!sdio->csd_dup.id_vec) {
 				cl_sub_dio_free(sdio);
 				sdio = NULL;
 				goto out;
 			}
-			memcpy((void *) sdio->csd_iter.__iov, iter->__iov,
-			       sizeof(struct iovec));
+			memcpy(sdio->csd_dup.id_vec, iter->__iov, vec_size);
+			sdio->csd_dup.id_vec_size = vec_size;
+			sdio->csd_iter.__iov = sdio->csd_dup.id_vec;
 		}
 	}
 out:
@@ -1335,11 +1349,10 @@ EXPORT_SYMBOL(cl_dio_aio_free);
 void cl_sub_dio_free(struct cl_sub_dio *sdio)
 {
 	if (sdio) {
-		void *tmp = (void *)sdio->csd_iter.__iov;
-
-		if (tmp) {
+		if (sdio->csd_dup.id_vec) {
 			LASSERT(sdio->csd_unaligned);
-			OBD_FREE_PTR(tmp);
+			csd_dup_free(&sdio->csd_dup);
+			sdio->csd_iter.__iov = NULL;
 		}
 		OBD_SLAB_FREE_PTR(sdio, cl_sub_dio_kmem);
 	}
@@ -1465,9 +1478,9 @@ EXPORT_SYMBOL(ll_release_user_pages);
 #endif
 
 /* copy IO data to/from internal buffer and userspace iovec */
-ssize_t ll_dio_user_copy(struct cl_sub_dio *sdio, struct iov_iter *write_iov)
+ssize_t ll_dio_user_copy(struct cl_sub_dio *sdio)
 {
-	struct iov_iter *iter = write_iov ? write_iov : &sdio->csd_iter;
+	struct iov_iter *iter = &sdio->csd_iter;
 	struct ll_dio_pages *pvec = &sdio->csd_dio_pages;
 	struct mm_struct *mm = sdio->csd_ll_aio->cda_mm;
 	loff_t pos = pvec->ldp_file_offset;
@@ -1596,14 +1609,6 @@ out:
 	LASSERTF(ergo(status == 0, i == pvec->ldp_count - 1),
 		 "status: %d, i: %d, pvec->ldp_count %zu, count %zu\n",
 		  status, i, pvec->ldp_count, count);
-
-	if (write_iov && status == 0) {
-		/* The copy function we use modifies the count in the iovec,
-		 * but that's actually the job of the caller, so we return the
-		 * iovec to the original count
-		 */
-		iov_iter_reexpand(iter, original_count);
-	}
 
 	if (mm_used)
 		kthread_unuse_mm(mm);

lustre/tests/rwv.c
@@ -59,6 +59,7 @@ static void usage(void)
 	printf("-a  append IO (O_APPEND)\n");
 	printf("-r  file read (O_RDONLY)\n");
 	printf("-w  file write (O_WRONLY)\n");
+	printf("-D  open file with (O_DIRECT)\n");
 	printf("-s  set the start pos of the read/write test\n");
 	printf("-z  test for read hitting hole\n");
 	printf("-d  create flags (O_LOV_DELAY_CREATE)\n");
@@ -100,7 +101,7 @@ int main(int argc, char **argv)
 	struct iovec *iov;
 	off64_t offset = 0;
 
-	while ((c = getopt(argc, argv, "f:n:s:rwahvdzo::")) != -1) {
+	while ((c = getopt(argc, argv, "f:n:s:rwahvdDzo::")) != -1) {
 		switch (c) {
 		case 'f':
 			fname = optarg;
@@ -137,6 +138,9 @@ int main(int argc, char **argv)
 			break;
 		case 'd':
 			flags |= O_LOV_DELAY_CREATE;
+			break;
+		case 'D':
+			flags |= O_DIRECT;
 			break;
 		case 'z':
 			pad = 0;

lustre/tests/sanity.sh
@@ -13918,6 +13918,20 @@ test_119i()
 }
 run_test 119i "test unaligned aio at varying sizes"
 
+test_119m() {
+	rwv -f $DIR/$tfile -Dw -n 3 0x7ffff 0x100001 0x180000 ||
+		error "DIO unaligned writev test failed"
+	rwv -f $DIR/$tfile -Dr -v -n 2 0x180000 0x100001 ||
+		error "DIO unaligned readv failed"
+	rm -f $DIR/$tfile
+	rwv -f $DIR/$tfile -Dw -n 3 0x80000 0x100000 0x180000 ||
+		error "DIO aligned writev test failed"
+	rwv -f $DIR/$tfile -Dr -v -n 2 0x180000 0x100000 ||
+		error "DIO aligned readv failed"
+	rm -f $DIR/$tfile
+}
+run_test 119m "Test DIO readv/writev: exercise iter duplication"
+
 test_120a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 	remote_mds_nodsh && skip "remote MDS with nodsh"