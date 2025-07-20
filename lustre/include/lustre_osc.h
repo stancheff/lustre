lustre/lustre-release

commit 262957eaef3daeeb4a1bf4f587d4d864fa8aaab1

LU-18691 quota: quota interop check for 64k page clients
When hitting the end of available quota a race condition can be hit
which allows an 64k unaligned I/O to be submitted and causes the
node to hang indefinitely.

This happens when a partial write hits quota limits and a subsequent
write is not aligned on 64k page boundary triggering a hang due to
64k vs 4k page aligned transfers.

HPE-bug-id: LUS-12724
Test-Parameters: testlist=sanity-quota clientarch=ppc64le clientdistro=el8.9 serverdistro=el9.4 env=ONLY=88,ONLY_REPEAT=10
Test-Parameters: testlist=sanity-quota clientarch=ppc64le clientdistro=el8.9 serverdistro=el8.9 serverversion=2.15.4 env=ONLY=88,ONLY_REPEAT=10
Test-Parameters: testlist=sanity-quota clientarch=aarch64 clientdistro=el9.3 serverdistro=el8.10 env=ONLY=88,ONLY_REPEAT=10
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I0f8638062f8b0e57207695c45e1fccbd7492c32d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57961
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: xinliang <xinliang.liu@linaro.org>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osc/osc_request.c
@@ -1553,19 +1553,15 @@ osc_brw_prep_request(int cmd, struct client_obd *cli, struct obdo *oa,
 	bool enable_checksum = true;
 	struct cl_page *clpage;
 	u64 foffset = 0;
+	u32 iop_pages = 0;
 
 	ENTRY;
 	if (pga[0]->bp_page) {
 		clpage = oap2cl_page(brw_page2oap(pga[0]));
 		inode = clpage->cp_inode;
-		if (clpage->cp_type == CPT_TRANSIENT) {
+		foffset = pga[0]->bp_off;
+		if (clpage->cp_type == CPT_TRANSIENT)
 			directio = true;
-			/* When page size interop logic is not supported by the
-			 * remote server use the old logic.
-			 */
-			if (imp_connect_unaligned_dio(cli->cl_import))
-				foffset = pga[0]->bp_off;
-		}
 	}
 	if (CFS_FAIL_CHECK(OBD_FAIL_OSC_BRW_PREP_REQ))
 		RETURN(-ENOMEM); /* Recoverable */
@@ -1789,6 +1785,12 @@ retry_encrypt:
 		goto no_bulk;
 	}
 
+	if (foffset)
+		iop_pages = interop_pages(foffset, page_count, pga);
+	/* need interop but server does not support, return failure */
+	if (iop_pages && !imp_connect_unaligned_dio(cli->cl_import))
+			GOTO(out, rc = -EINVAL); /* -EDQUOT? */
+
 	desc = ptlrpc_prep_bulk_imp(req, page_count,
 		cli->cl_import->imp_connect_data.ocd_brw_size >> LNET_MTU_BITS,
 		(opc == OST_WRITE ? PTLRPC_BULK_GET_SOURCE :
@@ -1800,8 +1802,8 @@ retry_encrypt:
 		GOTO(out, rc = -ENOMEM);
 	/* NB request now owns desc and will free it when it gets freed */
 	desc->bd_is_rdma = gpu;
-	if (directio && foffset)
-		desc->bd_md_offset = interop_pages(foffset, page_count, pga);
+	if (iop_pages)
+		desc->bd_md_offset = iop_pages;
 
 no_bulk:
 	body = req_capsule_client_get(pill, &RMF_OST_BODY);

lustre/tests/sanity-quota.sh
@@ -6592,6 +6592,38 @@ test_87()
 }
 run_test 87 "lfs quota -a should print default quota setting"
 
+# interop quota
+test_88()
+{
+	(($PAGE_SIZE > 4096)) || skip "require client with >4k pages"
+	setup_quota_test || error "setup quota failed with $?"
+
+	set_ost_qtype $QTYPE || error "enable ost quota failed"
+
+	$LFS setquota -u $TSTUSR -B 100M -i 0 $MOUNT ||
+		error "enable quota -B 100M failed."
+
+	local tfile
+	local result
+
+	local repeat=$(seq 10)
+	local arr=(1075761 1075770 1075800 1076000 1080000 1093000 2010000 \
+		   2080000 2095000 4096000)
+	[[ "$SLOW" = "no" ]] && repeat=1
+
+	for r in $repeat; do
+		for bs in ${arr[@]}; do
+			tfile=$DIR/$tdir/dd_largefile.${bs}
+			${RUNAS} dd if=/dev/urandom of=${tfile} bs=${bs} \
+				count=100 status=progress
+			rm -f ${tfile}
+		done
+	done
+
+	return 0
+}
+run_test 88 "Writing over quota should not hang"
+
 check_quota_no_mount()
 {
 	local opts="$1"

stancheff/lustre

commit 6470ffe486c809502bf44f49edb5f43160e73eb2

LU-19180 osc: split i/o for servers without unaligned_dio
When sending RPCs to older older servers without unaligned_dio
in cases where an RPC would be large and unaligned the request
can be failed -EINVAL.

However this is sub-optimal.

Instead the error should be unique to the failure and the caller
can check for the error, -EDEADLK, and split the RPC request
performing a short i/o and re-queuing the remaining pages.

Test-Parameters: serverversion=2.15
Fixes: 262957eaef ("LU-18691 quota: quota interop check for 64k page clients")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I63985ad380439cf9bbebdd7c3ef41fa96e3dfc63

lustre/include/lustre_osc.h
@@ -505,6 +505,7 @@ struct osc_brw_async_args {
 	int			 aa_requested_nob;
 	int			 aa_nio_count;
 	u32			 aa_page_count;
+	u32			 aa_excess;
 	s32			 aa_resends;
 	struct brw_page		**aa_ppga;
 	struct client_obd	*aa_cli;

lustre/osc/osc_cache.c
@@ -35,8 +35,6 @@ static int osc_make_ready(const struct lu_env *env, struct osc_async_page *oap,
 			  int cmd);
 static int osc_refresh_count(const struct lu_env *env, struct osc_object *osc,
 			     struct osc_async_page *oap, int cmd);
-static int osc_io_unplug_async(const struct lu_env *env,
-			       struct client_obd *cli, struct osc_object *osc);
 static void osc_free_grant(struct client_obd *cli, struct client_memcg *cmemcg,
 			   unsigned int nr_pages, unsigned int lost_grant,
 			   unsigned int dirty_grant);
@@ -347,6 +345,56 @@ static void osc_extent_free(struct kref *kref)
 	OBD_SLAB_FREE_PTR(ext, osc_extent_kmem);
 #endif
 }
+
+struct osc_extent *osc_extent_clone(struct osc_object *obj,
+				    struct osc_extent *base)
+{
+	struct osc_extent *ext;
+
+	ext = osc_extent_alloc(obj);
+	if (!ext)
+		RETURN(ext);
+
+	ext->oe_rw = base->oe_rw;
+	ext->oe_sync = base->oe_sync;
+	ext->oe_no_merge = base->oe_no_merge;
+	ext->oe_urgent = base->oe_urgent;
+	ext->oe_end = base->oe_end;
+	ext->oe_max_end = base->oe_max_end;
+	ext->oe_obj = obj;
+	ext->oe_srvlock = base->oe_srvlock;
+	ext->oe_ndelay = base->oe_ndelay;
+	ext->oe_dio = base->oe_dio;
+	ext->oe_csd = base->oe_csd;
+	if (base->oe_dlmlock)
+		ext->oe_dlmlock = ldlm_lock_get(base->oe_dlmlock);
+	ext->oe_is_rdma_only = base->oe_is_rdma_only;
+	ext->oe_mppr = base->oe_mppr;
+	ext->oe_layout_version = base->oe_layout_version;
+	ext->oe_grants = 0;
+	ext->oe_nr_pages = 0;
+
+	return ext;
+}
+EXPORT_SYMBOL(osc_extent_clone);
+
+void osc_extent_queue(struct osc_object *obj, struct osc_extent *ext, bool del)
+{
+	osc_object_lock(obj);
+	if (del)
+		list_del_init(&ext->oe_link);
+
+	if (!ext->oe_rw) { /* write */
+		list_add_tail(&ext->oe_link, &obj->oo_urgent_exts);
+		osc_update_pending(obj, OBD_BRW_WRITE, ext->oe_nr_pages);
+	} else {
+		list_add_tail(&ext->oe_link, &obj->oo_reading_exts);
+		osc_update_pending(obj, OBD_BRW_READ, ext->oe_nr_pages);
+	}
+	osc_extent_state_set(ext, OES_LOCK_DONE);
+	osc_object_unlock(obj);
+}
+EXPORT_SYMBOL(osc_extent_queue);
 
 static struct osc_extent *osc_extent_get(struct osc_extent *ext)
 {

lustre/osc/osc_internal.h
@@ -67,6 +67,9 @@ int osc_ladvise_base(struct obd_export *exp, struct obdo *oa,
 		     struct ladvise_hdr *ladvise_hdr,
 		     obd_enqueue_update_f upcall, void *cookie,
 		     struct ptlrpc_request_set *rqset);
+struct osc_extent *osc_extent_clone(struct osc_object *obj,
+				    struct osc_extent *ext);
+void osc_extent_queue(struct osc_object *obj, struct osc_extent *ext, bool del);
 int osc_process_config_base(struct obd_device *obd, struct lustre_cfg *cfg);
 int osc_build_rpc(const struct lu_env *env, struct client_obd *cli,
 		  struct list_head *ext_list, int cmd);

lustre/osc/osc_request.c
@@ -1518,7 +1518,7 @@ static inline u32 interop_pages(u64 foffset, u32 npgs, struct brw_page **pga)
 
 static int
 osc_brw_prep_request(int cmd, struct client_obd *cli, struct obdo *oa,
-		     u32 page_count, struct brw_page **pga,
+		     u32 page_count, u32 excess, struct brw_page **pga,
 		     struct ptlrpc_request **reqp, int resend)
 {
 	struct ptlrpc_request *req;
@@ -1774,7 +1774,7 @@ retry_encrypt:
 		iop_pages = interop_pages(foffset, page_count, pga);
 	/* need interop but server does not support, return failure */
 	if (iop_pages && !imp_connect_unaligned_dio(cli->cl_import))
-			GOTO(out, rc = -EINVAL); /* -EDQUOT? */
+		GOTO(out, rc = -EDEADLK); /* -EDQUOT? */
 
 	desc = ptlrpc_prep_bulk_imp(req, page_count,
 		cli->cl_import->imp_connect_data.ocd_brw_size >> LNET_MTU_BITS,
@@ -1985,6 +1985,7 @@ no_bulk:
 	aa->aa_requested_nob = requested_nob;
 	aa->aa_nio_count = niocount;
 	aa->aa_page_count = page_count;
+	aa->aa_excess = excess;
 	aa->aa_resends = 0;
 	aa->aa_ppga = pga;
 	aa->aa_cli = cli;
@@ -2448,7 +2449,7 @@ static int osc_brw_redo_request(struct ptlrpc_request *request,
 	rc = osc_brw_prep_request(lustre_msg_get_opc(request->rq_reqmsg) ==
 				OST_WRITE ? OBD_BRW_WRITE : OBD_BRW_READ,
 				  aa->aa_cli, aa->aa_oa, aa->aa_page_count,
-				  aa->aa_ppga, &new_req, 1);
+				  aa->aa_excess, aa->aa_ppga, &new_req, 1);
 	if (rc)
 		RETURN(rc);
 
@@ -2695,7 +2696,7 @@ static int brw_interpret(const struct lu_env *env,
 		       aa->aa_requested_nob :
 		       req->rq_bulk->bd_nob_transferred);
 
-	osc_release_ppga(aa->aa_ppga, aa->aa_page_count);
+	osc_release_ppga(aa->aa_ppga, aa->aa_page_count + aa->aa_excess);
 	ptlrpc_lprocfs_brw(req, transferred);
 
 	spin_lock(&cli->cl_loi_list_lock);
@@ -2757,6 +2758,7 @@ int osc_build_rpc(const struct lu_env *env, struct client_obd *cli,
 	int mpflag = 1;
 	int mem_tight = 0;
 	int page_count = 0;
+	int over = 0;
 	bool soft_sync = false;
 	bool ndelay = false;
 	bool srvlock = false;
@@ -2861,9 +2863,82 @@ int osc_build_rpc(const struct lu_env *env, struct client_obd *cli,
 		}
 	}
 
+resend:
 	sort_brw_pages(pga, page_count);
-	rc = osc_brw_prep_request(cmd, cli, oa, page_count, pga, &req, 0);
-	if (rc != 0) {
+	rc = osc_brw_prep_request(cmd, cli, oa, page_count, over, pga, &req, 0);
+	if (rc == -EDEADLK) {
+		struct osc_extent *at;
+		struct osc_extent *oe;
+		pgoff_t start;
+		pgoff_t end;
+		u64 foffset = pga[0]->bp_off;
+		int n;
+
+		for (n = 0; n < page_count; n++)
+			if (interop_pages(foffset, n, pga))
+				break;
+		i = --n;
+		oap = brw_page2oap(pga[i]);
+		start = osc_index(oap2osc(oap));
+		at = list_first_entry_or_null(ext_list, typeof(*at), oe_link);
+		if (!at)
+			GOTO(out, -EINVAL);
+		/* shift to the best extent to shift pages/grants from */
+		ext = osc_extent_clone(obj, at);
+		if (!ext)
+			GOTO(out, -ENOMEM);
+		ext->oe_start = start;
+		n = page_count;
+		for (; i < n; i++) {
+			oap = brw_page2oap(pga[i]);
+			end = osc_index(oap2osc(oap));
+			ext->oe_end = end;
+			list_del_init(&oap->oap_rpc_item);
+			list_del_init(&oap->oap_pending_item);
+			list_add_tail(&oap->oap_pending_item, &ext->oe_pages);
+		}
+		/* migrate grants and pages */
+		list_for_each_entry_safe(at, oe, ext_list, oe_link) {
+			int npages = 0;
+			u64 bytes = 0;
+			u64 moved = 0;
+			pgoff_t index;
+
+			start = -1;
+			end = 0;
+			list_for_each_entry(oap, &at->oe_pages,
+					    oap_pending_item) {
+				npages++;
+				bytes += oap->oap_count;
+				index = osc_index(oap2osc(oap));
+				if (index < start)
+					start = index;
+				if (index > end)
+					end = index;
+			}
+			if (at->oe_nr_pages > npages) {
+				if (at->oe_grants > bytes) {
+					moved = at->oe_grants - bytes;
+					grant -= moved;
+					ext->oe_grants += moved;
+				}
+				moved = at->oe_nr_pages - npages;
+				at->oe_nr_pages -= moved;
+				ext->oe_nr_pages += moved;
+				page_count -= moved;
+				over += moved;
+			}
+			/* re-queue extent if it has pages */
+			if (npages) {
+				at->oe_end = end;
+				at->oe_start = start;
+				at->oe_no_merge = 1;
+			}
+		}
+		osc_extent_queue(obj, ext, false);
+		goto resend;
+	}
+	if (rc) {
 		CERROR("%s: prep_req failed: rc = %d\n",
 		       cli->cl_import->imp_obd->obd_name, rc);
 		GOTO(out, rc);
@@ -2953,7 +3028,7 @@ out:
 			OBD_SLAB_FREE_PTR(oa, osc_obdo_kmem);
 		if (pga) {
 			osc_release_bounce_pages(pga, page_count);
-			osc_release_ppga(pga, page_count);
+			osc_release_ppga(pga, page_count + over);
 		}
 		/* this should happen rarely and is pretty bad, it makes the
 		 * pending list not follow the dirty order
@@ -2983,7 +3058,7 @@ void osc_send_empty_rpc(struct osc_object *osc, pgoff_t start)
 	/* For updated servers - don't do a read */
 	oa.o_flags = OBD_FL_NORPC;
 
-	rc = osc_brw_prep_request(OBD_BRW_READ, osc_cli(osc), &oa, 1, &pga,
+	rc = osc_brw_prep_request(OBD_BRW_READ, osc_cli(osc), &oa, 1, 0, &pga,
 				  &req, 0);
 
 	/* If we succeeded we ship it off, if not there's no point in doing